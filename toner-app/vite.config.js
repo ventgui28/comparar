347ba1e8a540193f8de25882794efa93fd6d7bd4
GabrielTeixeiral0lGabrielTeixeiral0l <teixeiramarvao@gmail.com>
2026-05-15 10:06:41 +0100

    build(tauri): adicionar configuração e plugins do Tauri v2
    Atualiza as permissões no capabilities, adiciona plugins dialog e fs, e configura o vite para integração com o Tauri.

--- toner-app/vite.config.js
+++ toner-app/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Prevent Vite from clearing the screen
+  // so that Tauri output is not cleared
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [react()],
 })
